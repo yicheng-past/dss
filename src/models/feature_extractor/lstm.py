c78a3e42570a100f2fa6891437f3707278315f54
YichengShen <jsszsyc@hotmail.com>
2023-11-28 21:19:55 -0500

    Update lstm feature extractor

--- src/models/feature_extractor/lstm.py
+++ src/models/feature_extractor/lstm.py
@@ -16,13 +16,13 @@ class LSTMFeatureExtractor(nn.Module):
     ):
         super().__init__()
         self.fc = nn.Linear(in_channels, hidden_size)
-        self.conv = nn.Conv1d(
-            in_channels=hidden_size,
-            out_channels=hidden_size,
-            kernel_size=stride,
-            stride=stride,
-            padding=1,
-        )
+        # self.conv = nn.Conv1d(
+        #     in_channels=hidden_size,
+        #     out_channels=hidden_size,
+        #     kernel_size=stride,
+        #     stride=stride,
+        #     padding=1,
+        # )
         self.height = hidden_size * (2 if bidirectional else 1)
         self.lstm = nn.LSTM(
             input_size=hidden_size,
@@ -45,15 +45,17 @@ class LSTMFeatureExtractor(nn.Module):
         Returns:
             torch.Tensor: (batch_size, out_chans, height, time_steps)
         """
-        # x: (batch_size, in_channels, time_steps)
-        x = self.fc(x.transpose(1, 2))  # x: (batch_size, time_steps, hidden_size)
-        x = self.conv(x.transpose(1, 2))  # x: (batch_size, hidden_size, time_steps)
+        # # x: (batch_size, in_channels, time_steps)
+        # x = self.fc(x.transpose(1, 2))  # x: (batch_size, time_steps, hidden_size)
+        # x = self.conv(x.transpose(1, 2))  # x: (batch_size, hidden_size, time_steps)
 
         if self.out_size is not None:
             x = x.unsqueeze(1)  # x: (batch_size, 1, hidden_size, time_steps)
             x = self.pool(x)  # x: (batch_size, 1, hidden_size, output_size)
             x = x.squeeze(1)  # x: (batch_size, hidden_size, output_size)
         x = x.transpose(1, 2)  # x: (batch_size, output_size, hidden_size)
+
+        x = self.fc(x)
         x, _ = self.lstm(x)  # x: (batch_size, output_size, hidden_size * num_directions)
         x = x.transpose(1, 2)  # x: (batch_size, hidden_size * num_directions, output_size)
         x = x.unsqueeze(1)  # x: (batch_size, out_chans, hidden_size * num_directions, time_steps)
