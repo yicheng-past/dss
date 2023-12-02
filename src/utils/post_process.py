5cdac801d78555f35d68abddfcbcb73b5c387a0d
YichengShen <jsszsyc@hotmail.com>
2023-12-01 22:36:25 -0500

    Add nms

--- src/utils/post_process.py
+++ src/utils/post_process.py
@@ -3,6 +3,22 @@ import polars as pl
 from scipy.signal import find_peaks
 
 
+def non_maximum_suppression(df, iou_threshold=100):
+    grouped = df.groupby(['event', 'series_id'])
+    nms_predictions = []
+
+    for _, group in grouped:
+        sorted_group = group.sort('score', reverse=True)
+
+        while not sorted_group.is_empty():
+            highest = sorted_group[0]
+            nms_predictions.append(highest)
+            sorted_group = sorted_group.filter((sorted_group['step'] - highest['step']).abs() > iou_threshold)
+
+    nms_df = pl.DataFrame(nms_predictions).sort('row_id').with_column(pl.arange(0, len(nms_predictions)).alias('index')).reset_index(drop=True)
+    return nms_df
+
+
 def post_process_for_seg(
     keys: list[str], preds: np.ndarray, score_th: float = 0.01, distance: int = 5000
 ) -> pl.DataFrame:
@@ -52,4 +68,6 @@ def post_process_for_seg(
     sub_df = pl.DataFrame(records).sort(by=["series_id", "step"])
     row_ids = pl.Series(name="row_id", values=np.arange(len(sub_df)))
     sub_df = sub_df.with_columns(row_ids).select(["row_id", "series_id", "step", "event", "score"])
+
+    sub_df = non_maximum_suppression(sub_df)
     return sub_df
